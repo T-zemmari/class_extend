1c87ef1696e63bc1cd7b187471ef02835062d651
Tarek <tarek@tedybusiness.com>
2023-04-24 09:49:26 +0200

    clase product, se han añadido dos campo mass (categoria y subcategoria)

--- includes/product_class.php
+++ includes/product_class.php
@@ -8,14 +8,17 @@ class Product
     protected $cantidad;
     protected $fecha_creacion;
     protected $user_id;
+    protected $categoria;
+    protected $sub_categria;
 
-    function __construct($nombre, $fecha_creacion, $cantidad, $user_id)
+    function __construct($nombre, $categoria,$sub_categria,$fecha_creacion, $cantidad, $user_id)
     {
-
         $this->nombre = $nombre;
         $this->fecha_creacion = $fecha_creacion;
         $this->cantidad = $cantidad;
         $this->user_id = $user_id;
+        $this->categoria = $categoria;
+        $this->sub_categria = $sub_categria;
     }
     function getNombre()
     {
@@ -38,7 +41,7 @@ class Product
     {
         return $this->fecha_creacion;
     }
-    function serFechaCreacion($fecha_creacion)
+    function setFechaCreacion($fecha_creacion)
     {
         $this->fecha_creacion = $fecha_creacion;
     }
@@ -46,8 +49,24 @@ class Product
     {
         return $this->user_id;
     }
-    function serUserId($user_id)
+    function setUserId($user_id)
     {
         $this->user_id = $user_id;
+    }
+    function getUCategoria()
+    {
+        return $this->categoria;
+    }
+    function setCategoria($categoria)
+    {
+        $this->categoria = $categoria;
+    }
+    function getSubCategoria()
+    {
+        return $this->sub_categria;
+    }
+    function setSubCatgoria($sub_categria)
+    {
+        $this->sub_categria = $sub_categria;
     }
 }
