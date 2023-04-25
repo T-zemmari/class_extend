a6d569a186e6dceb397d0a84f99a5a3b88e1401e
Tarek <tarek@tedybusiness.com>
2023-04-25 15:09:47 +0200

    funciones insert y update producto preparadas

--- includes/product_class.php
+++ includes/product_class.php
@@ -3,7 +3,7 @@
 
 class Product
 {
-
+    protected $id;
     protected $nombre;
     protected $cantidad;
     protected $fecha_creacion;
@@ -11,7 +11,7 @@ class Product
     protected $categoria;
     protected $sub_categoria;
 
-    function __construct($nombre, $categoria,$sub_categoria,$fecha_creacion, $cantidad, $user_id)
+    function __construct($nombre, $categoria, $sub_categoria, $fecha_creacion, $cantidad, $user_id)
     {
         $this->nombre = $nombre;
         $this->fecha_creacion = $fecha_creacion;
@@ -19,6 +19,14 @@ class Product
         $this->user_id = $user_id;
         $this->categoria = $categoria;
         $this->sub_categoria = $sub_categoria;
+    }
+    function setId($id)
+    {
+        $this->id = $id;
+    }
+    function getId()
+    {
+        return $this->id;
     }
     function getNombre()
     {
@@ -68,5 +76,28 @@ class Product
     function setSubCategoria($sub_categoria)
     {
         $this->sub_categoria = $sub_categoria;
+    }
+    function guardar_producto()
+    {
+        $mensaje = "";
+        $respuesta = [];
+        if (empty($datos)) $mensaje = "Faltan datos";
+        $sql = "INSERT INTO producto SET nombre='" . mysql_esc($this->nombre) . "',user_id='" . mysql_esc($this->user_id) . "',cantidad='" . mysql_esc($this->cantidad) . "',categoria='" . mysql_esc($this->categoria) . "',sub_categoria='" . mysql_esc($this->sub_categoria) . "',fecha_creacion=NOW()";
+        if (mysql_query($sql)) $id_producto = mysql_insert_id();
+        $respuesta['mensaje'] = $mensaje;
+        $respuesta['last_id'] = $id_producto;
+        return $respuesta;
+    }
+    function actualizar_producto()
+    {
+        $mensaje = "";
+        $respuesta = [];
+        if (empty($datos)) $mensaje = "Faltan datos";
+        $sql = "UPDATE producto SET nombre='" . mysql_esc($this->nombre) . "',user_id='" . mysql_esc($this->user_id) . "',cantidad='" . mysql_esc($this->cantidad) . "',categoria='" . mysql_esc($this->categoria) . "',sub_categoria='" . mysql_esc($this->sub_categoria) . "' WHERE id='" . mysql_esc($this->id) . "' ";
+        if (mysql_query($sql)) {
+            $respuesta['mensaje'] = $mensaje;
+            $respuesta['products_id'] = $this->id;
+        }
+        return $respuesta;
     }
 }
